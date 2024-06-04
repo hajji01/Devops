76b5206b66bced68e00cc9a2c65eb0c5ffd041d7
Zakaria HAJJI <98238522+hajji01@users.noreply.github.com>
2024-06-04 19:09:43 +0200

    Ajout Boucle

--- organiser.sh
+++ organiser.sh
@@ -1,7 +1,14 @@
 mkdir -p organised_files
+
+# Boucle pour parcourir tous les fichiers dans le répertoire courant
 for file in *; do
-  if [ -f "" ]; then
-    mv "" organised_files/
+  if [ -f "$file" ]; then
+    # Extraire l'extension du fichier
+    extension="${file##*.}"
+    # Créer un répertoire basé sur l'extension
+    mkdir -p "organised_files/$extension"
+    
+    # Déplacer le fichier dans le répertoire approprié
+    mv "$file" "organised_files/$extension/"
   fi
 done
-ls -l organised_files
