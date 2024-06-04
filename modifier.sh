19d48fdf9e9c7e7e876ac375f4c06ca620957515
Zakaria HAJJI <98238522+hajji01@users.noreply.github.com>
2024-06-04 19:12:54 +0200

    Ajout des modifs

--- modifier.sh
+++ modifier.sh
@@ -1,5 +1,18 @@
-for file in organised_files/*; do
-  if [ -f "" ]; then
-    echo "Modification" >> ""
+
+# Boucle pour parcourir tous les fichiers dans organiser_sh
+for file in organised_files/*/*; do
+  # Vérifier si c'est un fichier
+  if [ -f "$file" ]; then
+    echo "Modification" >> "$file"
   fi
 done
+
+# Afficher les fichiers modifiés pour vérification
+
+echo "Fichiers modifiés :"
+
+
+
+
+#Affichage des permissions
+ls -l organised_files/*
